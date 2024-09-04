d806919ab6771f2e538695ce6c17ffa946b6da82
rejohn <rejohn@rejohn>
2024-09-04 14:59:13 -0400

    Add dashboard component

--- frontend/src/app/(app)/layout.js
+++ frontend/src/app/(app)/layout.js
@@ -4,18 +4,38 @@ import { useAuth } from '@/hooks/auth'
 import Navigation from '@/app/(app)/Navigation'
 import Loading from '@/app/(app)/Loading'
 
-const AppLayout = ({ children }) => {
+import { useState } from "react";
+import Sidebar from '@/components/sidebar';
+import SidebarV2 from '@/components/sidebar/SidebarV2';
+import Overlay from '@/components/overlay';
+import ProtoTypes from "prop-types";
+import HeaderOne from '@/components/header/HeaderOne';
+import HeaderTwo from '@/components/header/HeaderTwo';
+
+const AppLayout = ({ bg, overlay, children }) => {
     const { user } = useAuth({ middleware: 'auth' })
+    const [sidebar, setSidebar] = useState(true);
 
     if (!user) {
         return <Loading />
     }
 
     return (
-        <div className="min-h-screen bg-gray-100">
-            <Navigation user={user} />
-
-            <main>{children}</main>
+        <div className={`layout-wrapper ${sidebar && "active"}  w-full`}>
+            <div className="relative flex w-full">
+                <Sidebar handleActive={() => setSidebar(!sidebar)} />
+                {overlay ? overlay : <Overlay />}
+                <SidebarV2 />
+                <div
+                className={`body-wrapper flex-1 overflow-x-hidden ${
+                    bg ? bg : "dark:bg-darkblack-500"
+                } `}
+                >
+                <HeaderOne handleSidebar={() => setSidebar(!sidebar)} />
+                <HeaderTwo handleSidebar={() => setSidebar(!sidebar)} />
+                {children}
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,21 +1,65 @@
-import { Nunito } from 'next/font/google'
-import '@/app/global.css'
+// import { Nunito } from 'next/font/google'
+// import '@/app/global.css'
 
-const nunitoFont = Nunito({
-    subsets: ['latin'],
-    display: 'swap',
-})
+// const nunitoFont = Nunito({
+//     subsets: ['latin'],
+//     display: 'swap',
+// })
+
+"use client";
+import '@/app/global.css'
+import "/public/static/css/style.css";
+import "/public/static/css/font-awesome-all.min.css";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+// import "quill/dist/quill.snow.css";
+import { useEffect, useState } from "react";
+import { createContext } from "react";
+import ServiceWorker from '@/components/sw/sw';
+
+
+export const ThemeContext = createContext(null);
 
 const RootLayout = ({ children }) => {
+   const [theme, setTheme] = useState();
+    useEffect(() => {
+        setTheme(
+        localStorage.getItem("theme") === "" || localStorage.getItem("theme")
+            ? localStorage.getItem("theme")
+            : ""
+        );
+    }, []);
+    useEffect(() => {
+        if (
+        localStorage.getItem("theme") !== "" &&
+        localStorage.getItem("theme") !== "dark"
+        ) {
+        localStorage.setItem("theme", "");
+        }
+        document.querySelector("html").classList = localStorage.getItem("theme");
+    }, []);
+
     return (
-        <html lang="en" className={nunitoFont.className}>
-            <body className="antialiased">{children}</body>
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ServiceWorker />
+        <html lang="en">
+            <head>
+            <link rel="manifest" href="/manifest.json" />
+            <meta
+                name="viewport"
+                content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+            />
+            </head>
+            <body>{children}</body>
         </html>
-    )
+        </ThemeContext.Provider>
+    );
 }
 
-export const metadata = {
-    title: 'Note App',
-}
+// export const metadata = {
+//     title: 'Note App',
+// }
 
 export default RootLayout
