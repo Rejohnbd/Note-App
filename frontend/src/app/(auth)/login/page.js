0eeebe3e99dd80dfd4cefd0b3bcbf5916ef2ddda
rejohn <rejohn@rejohn>
2024-09-04 13:39:53 -0400

    Modify Registration page

--- frontend/src/app/(auth)/login/page.js
+++ frontend/src/app/(auth)/login/page.js
@@ -91,6 +91,7 @@ const Login = () => {
                                 className="text-bgray-800 text-base border border-bgray-300 dark:border-darkblack-400 dark:bg-darkblack-500 dark:text-white h-14 w-full focus:border-success-300 focus:ring-0 rounded-lg px-4 py-3.5 placeholder:text-bgray-500 placeholder:text-base"
                                 onChange={event => setPassword(event.target.value)}
                                 required
+                                placeholder="Password"
                                 autoComplete="current-password"
                             />
                             <InputError messages={errors.password} className="mt-2" />
